2e17875f990dc58abf445334bd7ca5ef2cd1ab6a
SMRITI SAMIKSHYA MISHRA <smritisam2k20@gmail.com>
2026-03-27 23:50:35 +0530

    changes for deployment

---
+++ multiplayer-tictactoe/nakama/start-nakama.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -eu
+
+/nakama/nakama migrate up --database.address "$DATABASE_URL"
+
+exec /nakama/nakama \
+  --name nakama1 \
+  --database.address "$DATABASE_URL" \
+  --logger.level INFO \
+  --session.token_expiry_sec 7200 \
+  --runtime.path /nakama/data/modules \
+  --socket.server_key "${NAKAMA_SERVER_KEY}" \
+  --runtime.http_key "${NAKAMA_HTTP_KEY}"
